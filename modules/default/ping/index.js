3a94c1060d81a4bbc5195011dda6872f5bc9c821
Nick Konecny <nkonecny@stashinvest.com>
2017-12-10 12:26:18 -0500

    Added bash ping ability

--- modules/default/ping/index.js
+++ modules/default/ping/index.js
@@ -1,4 +1,6 @@
 'use strict';
+var exec = require('child_process').exec;
+
 
 module.exports = class Ping extends BaseModule {
   handle(data) {
@@ -7,7 +9,34 @@ module.exports = class Ping extends BaseModule {
       return;
     }
 
+    if (data.cmd === "ding") {
+      this.bot.postMessage(data.channel, 'dong');
+      return;
+    }
+
+    if (data.user_text) {
+      let url = data.user_text.replace("<", "");
+      url = url.replace(">", "");
+      url = url.replace("http://", "");
+      url = url.replace("https://", "");
+      
+      if (url.includes("|")) {
+        url = url.split("|")[0];
+      }
+      
+      this.ping(data, url);
+      return;
+    }
+    
+
     this.bot.postMessage(data.channel, 'pong');
+  }
+
+  ping(data, url) {    
+    this.bot.postMessage(data.channel, "Pinging: " + url);      
+    exec("ping -c 3 " + url, (error, stdout, stderr) => {
+      this.bot.postMessage(data.channel, stderr ? stderr : stdout);      
+    });
   }
 
   help() {
