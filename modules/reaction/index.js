7fa59748e6122b0512e696ee003b7139ca9be288
Nick Konecny <nkonecny@stashinvest.com>
2018-02-12 15:47:02 -0500

    Debounce plus

--- modules/reaction/index.js
+++ modules/reaction/index.js
@@ -1,16 +1,23 @@
 'use strict';
+const cache = require('memory-cache');
 
 module.exports = class Reactions extends BaseModule {
   async handle(data, modules) {
-    // Heavy plus will plus user!
+    // Heavy plus will plus user!  
     if (
       data.reaction === 'heavy_plus_sign' &&
       data.item_user &&
       data.item_user !== data.user
-    ) {
+    ) {      
+      if (cache.get(this.getPlusKey(data)) != null){
+        // try to dup pluses
+        return;
+      }
+
       const module = modules['plus'];
       const userName = await module.bot.getUserNameFromId(data.item_user);
       module.plusUser(data.item.channel, userName.user.name);
+      cache.put(this.getPlusKey(data), '', 5 * 60 * 1000, ()=>{});
     }
     
     if (data.reaction === "eggplant") {      
@@ -18,6 +25,12 @@ module.exports = class Reactions extends BaseModule {
     }
     
   }
+
+
+  getPlusKey(data) {
+    return `${data.item_user}${data.user}${data.item.channel}${data.item.reaction}`;
+  }
+
 
   getType() {
     return BaseModule.TYPES.REACTIONS;
