ee0d1fc5364ffbb78aa12de5fae21c522a3c173d
Nick Konecny <nkonecny@stashinvest.com>
2018-02-14 10:49:49 -0500

    Crypto

---
+++ modules/crypto/index.js
@@ -0,0 +1,79 @@
+const request = require('request');
+const util = require('util');
+
+const BASE_URL = "https://min-api.cryptocompare.com/data/price?fsym=%s&tsyms=USD"
+
+const bitcoin = "BTC";
+const ethereum = "ETH";
+const lightCoin = "LTC";
+
+module.exports = class Crypto extends BaseModule {
+  async handle(data) {
+	this.postFancyData(data);
+  }
+
+
+  async postFancyData(data) {  
+  	const bitcoinPrice = await this.getCryptoPrice(bitcoin);
+  	const ethereumPrice = await this.getCryptoPrice(ethereum);
+  	const lightCoinPrice = await this.getCryptoPrice(lightCoin);
+    
+    const fields = [
+      {
+              "title": "Bitcoin:",
+              "value": bitcoinPrice.USD,
+              "short": false
+          },
+          {
+              "title": "Ethereum:",
+              "value": ethereumPrice.USD,
+              "short": false
+          },
+          { 
+              "title": "LightCoin:",
+              "value": lightCoinPrice.USD,
+              "short": false
+          }
+          
+    ]
+
+    this.bot.postRawMessage(
+        data.channel,
+        {
+          "icon_emoji": ":moneybag:",
+          "username": "CryptoCat",
+          "attachments": [
+              {
+                  "color": "#90c564",
+                  "fields": fields,
+                  "footer": "brochaco",                
+              }
+          ]
+        }
+      );
+  
+  }
+
+  getCryptoPrice(id) {
+    var options = {
+      url: util.format(BASE_URL, id),      
+    };
+
+    return new Promise((resolve, reject) => {
+      request(options, (error, response, body) => {
+        if (error) {
+          reject(error);
+          console.error("error", error);
+          return;
+        }
+
+        resolve(JSON.parse(body));
+      });
+    });  
+  }
+
+
+  help() {
+    return 'Pager duty command will return the on call list.';
+  }
+}
