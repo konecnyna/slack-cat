6a8b2605f009b41dd5e32318c37be601008b4864
Nick Konecny <nkonecny@stashinvest.com>
2018-03-03 10:57:10 -0500

    Added welcome cat to core modules

---
+++ modules/welcomecat/welcome-helper.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = class WeclomeHelper {
+  constructor(model) {
+    this.model = model;
+  }
+
+  async setMessage(context, data) {
+    const message = await context.upsert(
+      this.model,
+      { where: { channel: data.channel } },
+      {
+        channel: data.channel,
+        message: data.user_text,
+      },
+      {
+        message: data.user_text,
+      }
+    );    
+    return await message.get('message');
+  }
+
+  async updateModel(channel, fieldName, value) {
+    const fields = {};
+    fields[fieldName] = value;
+
+    this.model.update(fields, {
+      where: { channel: channel }
+    });
+  }
+
+  async getOptionsForChannel(channel) {
+    const welcomeObject = await this.model.findOne({
+      where: {
+        channel: channel
+      },
+    });
+
+  
+    return welcomeObject;
+  }
+}
