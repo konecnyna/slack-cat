058c24bb3052680e5b9ff7c9131e3d747d7d5abd
Nick Konecny <nkonecny@etsy.com>
2018-03-06 18:32:10 -0500

    Added poop board

--- modules/poop/index.js
+++ modules/poop/index.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const util = require('util');
 const botParams = {
   icon_emoji: ':poop:',
   username: 'PoopCat',
@@ -8,16 +7,15 @@ const botParams = {
 
 module.exports = class Poop extends BaseStorageModule {
   async handle(data) {
+
+    if (data.cmd === 'poop-board') {
+      this.postLeaderBoard(data);
+      return;
+    }
+
     const poopData = await this.updatePoop(data);
-
-    const output = util.format(
-      '*%s just pooped!* _%s pooped %d times_.\n Please lock your screen next time! http://osxdaily.com/2011/01/17/lock-screen-mac/ \n',
-      poopData.user,
-      poopData.user,
-      poopData.poops
-    );
-
-    this.bot.postMessageWithParams(data.channel, output, botParams);
+    const msg = `${poopData.user} just pooped!* ${poopData.user} pooped ${poopData.poops} times_.\n Please lock your screen next time! http://osxdaily.com/2011/01/17/lock-screen-mac/ \n`;
+    this.bot.postMessageWithParams(data.channel, msg, botParams);
   }
 
   registerSqliteModel() {
@@ -49,6 +47,36 @@ module.exports = class Poop extends BaseStorageModule {
       user: userName,
       poops: poop.get('poops'),
     };
+  }
+
+  async postLeaderBoard(data) {
+      const poops = await this.PoopModel.findAll({
+        order: [['poops', 'DESC']],
+        limit: 10,
+      });
+
+      const fields = [];
+      poops.forEach((plus, index) => {
+        fields.push({
+          title: `${index + 1}. ${plus.get('name')} (${plus.get('poops')} poops)`,
+          short: false,
+        });
+      });
+
+      this.bot.postRawMessage(data.channel, {
+        icon_emoji: ':poop:',
+        username: 'PoopBoard',
+        attachments: [
+          {
+            color: '#795548',
+            fields: fields,
+          },
+        ],
+      });
+  }
+
+  aliases() {
+    return ['poop-board'];
   }
 
   help() {
