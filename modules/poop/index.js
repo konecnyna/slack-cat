c01068fb9dc21f391220cac025628a7ab69baaeb
Nick Konecny <nkonecny@stashinvest.com>
2018-03-08 16:21:13 -0500

    Fixed poop name

--- modules/poop/index.js
+++ modules/poop/index.js
@@ -30,7 +30,7 @@ module.exports = class Poop extends BaseStorageModule {
 
   async updatePoop(data) {
     const user = await this.bot.getUserNameFromId(data.user);
-    const userName = user.user.name;
+    const userName = (user.profile) ? user.profile.display_name : user.name;
     const poop = await this.upsert(
       this.PoopModel,
       { where: { name: userName } },
