b4c0292ceea832a5cb315be20128a1d8bff3cc5a
Nick Konecny <nkonecny@stashinvest.com>
2018-05-18 15:13:54 -0400

    Added first poop msg

--- modules/poop/index.js
+++ modules/poop/index.js
@@ -13,8 +13,17 @@ module.exports = class Poop extends BaseStorageModule {
       return;
     }
 
+    this.updateAndPostPoop(data);
+  }
+
+  async updateAndPostPoop(data) {
     const poopData = await this.updatePoop(data);
-    const msg = `*${poopData.user} just got pooped!!!* ${poopData.user} pooped _${poopData.poops} times_.\nPlease lock your screen next time! http://osxdaily.com/2011/01/17/lock-screen-mac/\n`;
+    let msg = `*${poopData.user} just got pooped!!!* ${poopData.user} pooped _${poopData.poops} times_.\nPlease lock your screen next time! http://osxdaily.com/2011/01/17/lock-screen-mac/\n`;
+    if (poopData.poops === 1) {
+      msg = `*${poopData.user}* got pooped for the first time. That's a big stink! Don't leave your computer unlocked when you're not with it! Side effects can include hacking, stealing, destruction, and...pooping (by one of your sneaky gremlin coworkers). http://osxdaily.com/2011/01/17/lock-screen-mac/`;      
+    }
+
+  
     this.bot.postMessageWithParams(data.channel, msg, botParams);
   }
 
