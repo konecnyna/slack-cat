6b171eb3968984e38360e3e6e094e786652927a9
Nick Konecny <nkonecny@stashinvest.com>
2018-05-21 15:45:13 -0400

    Added extended

--- modules/weather/index.js
+++ modules/weather/index.js
@@ -12,44 +12,46 @@ const botParams = {
 };
 
 const radarMap = {
-  "AR":"lit",
-  "AZ":"prc",
-  "CA":"bfl",
-  "CO":"den",
-  "CT":"hfd",
-  "FL":"eyw",
-  "FL":"pie",
-  "GA":"csg",
-  "IA":"dsm",
-  "ID":"myl",
-  "IL":"spi",
-  "KS":"sln",
-  "KY":"bwg",
-  "LA":"msy",
-  "MI":"cad",
-  "MN":"stc",
-  "MO":"jef",
-  "MS":"tvr",
-  "MT":"lwt",
-  "NC":"clt",
-  "ND": "bis",
-  "NE":"lbf",
-  "NH":"bml",
-  "NM":"row",
-  "NV": "rno",
-  "NY":"bgm",
-  "OH": "day",
-  "OK":"law",
-  "OR":"rdm",
-  "SD":"pir",
-  "TX":"bro",
-  "TX":"sat",
-  "UT": "pvu",
-  "VA": "fcx",
-  "WA": "tiw",
-  "WY": "riw",
-}
+  AR: 'lit',
+  AZ: 'prc',
+  CA: 'bfl',
+  CO: 'den',
+  CT: 'hfd',
+  FL: 'eyw',
+  FL: 'pie',
+  GA: 'csg',
+  IA: 'dsm',
+  ID: 'myl',
+  IL: 'spi',
+  KS: 'sln',
+  KY: 'bwg',
+  LA: 'msy',
+  MI: 'cad',
+  MN: 'stc',
+  MO: 'jef',
+  MS: 'tvr',
+  MT: 'lwt',
+  NC: 'clt',
+  ND: 'bis',
+  NE: 'lbf',
+  NH: 'bml',
+  NM: 'row',
+  NV: 'rno',
+  NY: 'bgm',
+  OH: 'day',
+  OK: 'law',
+  OR: 'rdm',
+  SD: 'pir',
+  TX: 'bro',
+  TX: 'sat',
+  UT: 'pvu',
+  VA: 'fcx',
+  WA: 'tiw',
+  WY: 'riw',
+};
 
+const DAYS = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    
 module.exports = class Weather extends BaseModule {
   async handle(data) {
     if (this.aliases().includes(data.cmd)) {
@@ -77,11 +79,11 @@ module.exports = class Weather extends BaseModule {
     }
 
     darksky
-      .latitude(coords.lat) // required: latitude, string || float.
-      .longitude(coords.lng) // required: longitude, string || float.
-      .exclude('minutely,daily') // optional: exclude, string || array, refer to API documentation.
-      .extendHourly(true) // optional: extend, boolean, refer to API documentation.
-      .get() // execute your get request.
+      .latitude(coords.lat)
+      .longitude(coords.lng)
+      .exclude('minutely')
+      .extendHourly(true)
+      .get()
       .then(weatherData => {
         this.sendFormattedWeather(data, weatherData, coords.name);
       })
@@ -94,18 +96,26 @@ module.exports = class Weather extends BaseModule {
   handleRadar(data) {
     let img = '';
     if (!data.user_text) {
-      this.bot.postMessage(data.channel, "http://images.intellicast.com/WxImages/RadarLoop/usa_None_anim.gif");
+      this.bot.postMessage(
+        data.channel,
+        'http://images.intellicast.com/WxImages/RadarLoop/usa_None_anim.gif'
+      );
       return;
     }
 
     const location = radarMap[data.user_text.toUpperCase()];
     if (location) {
       img = `http://images.intellicast.com/WxImages/RadarLoop/${location}_None_anim.gif`;
-    } else {
-      img = 'http://images.intellicast.com/WxImages/RadarLoop/usa_None_anim.gif';  
+      this.bot.postMessage(data.channel, img);
+      return;
     }
-    
-    this.bot.postMessage(data.channel, img);
+
+    this.bot.postMessage(
+      data.channel,
+      `Couldn't find ${data.user_text}. Maps: \`\`\`${Object.keys(
+        radarMap
+      ).join('\n')}\`\`\``
+    );
   }
 
   help() {
@@ -120,9 +130,14 @@ module.exports = class Weather extends BaseModule {
   }
 
   sendFormattedWeather(data, weatherData, location) {
-    const title = `Currently in ${location}: ${parseInt(weatherData.currently.apparentTemperature)}°`;
-            
-    
+    if (data.args && data.args.includes('--extended')) {
+      this.handleExtendedForecast(data, weatherData, location);
+      return;
+    }
+
+    const title = `Currently in ${location}: ${parseInt(
+      weatherData.currently.apparentTemperature
+    )}°`;
 
     const summary = util.format(
       'Forecast: %s for the hour. %s\n\n %d%% humidity with wind speed of %d MPH',
@@ -132,7 +147,7 @@ module.exports = class Weather extends BaseModule {
       weatherData.currently.windSpeed
     );
 
-    const emoji = this.weatherEmoji(weatherData.hourly.icon);    
+    const emoji = this.weatherEmoji(weatherData.hourly.icon);
 
     this.bot.postFancyMessage(
       data.channel,
@@ -144,30 +159,55 @@ module.exports = class Weather extends BaseModule {
     );
   }
 
-  weatherEmoji(icon) {    
+  handleExtendedForecast(data, weatherData, location) {
+    // Day, weather
+    const fields = [];
 
-  	switch(icon) {
-  		case "clear-day":
-  			return ":sunny:";  			
-  		case "clear-night":
-  			return ":new_moon:";  		
-		case "snow":
-		case "sleet":
-			return ":snow_cloud:";		
-		case "wind":
-			return ":wind_blowing_face:";
-		case "fog":
-			return ":foggy:";
-		case "cloudy":
-			return ":cloud:";
-		case "partly-cloudy-day":
-			return ":sun_small_cloud:";
-		case "partly-cloudy-night":
-			return ":partly_sunny:";
-		case "rain":
-			return ":rain_cloud:";
-		default:
-			return ":sunny:";
-  	}  	
+    for (let i = 0; i < 4; i++) {
+      const day = weatherData.daily.data[i];
+      const weatherDate = new Date(day.time*1000);
+      fields.push({
+        title: DAYS[weatherDate.getDay()],
+        value: `${this.weatherEmoji(day.icon)}`,
+        short: true,
+      });
+    }
+
+    this.bot.postRawMessage(data.channel, {
+      icon_emoji: ':moon:',
+      username: 'WeatherCat',
+      attachments: [
+        {
+          color: "#ddd",
+          fields: fields,          
+        },
+      ],
+    });
+  }
+
+  weatherEmoji(icon) {
+    switch (icon) {
+      case 'clear-day':
+        return ':sunny:';
+      case 'clear-night':
+        return ':new_moon:';
+      case 'snow':
+      case 'sleet':
+        return ':snow_cloud:';
+      case 'wind':
+        return ':wind_blowing_face:';
+      case 'fog':
+        return ':foggy:';
+      case 'cloudy':
+        return ':cloud:';
+      case 'partly-cloudy-day':
+        return ':sun_small_cloud:';
+      case 'partly-cloudy-night':
+        return ':partly_sunny:';
+      case 'rain':
+        return ':rain_cloud:';
+      default:
+        return ':sunny:';
+    }
   }
 };
