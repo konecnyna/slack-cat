fa5d1cd2d37be1d3c2895d4d0b08a4dcc8b4c2b7
Nick Konecny <nkonecny@stashinvest.com>
2018-06-22 09:09:46 -0400

    Added error msg for choose

--- modules/choose/index.js
+++ modules/choose/index.js
@@ -1,26 +1,28 @@
 'use strict';
 
-
 module.exports = class Choose extends BaseModule {
   async handle(data) {
-  	try {  		
-    	const userData = await this.bot.getChannelById(data.channel);
-    	const randUser = this.getRandomUser(userData);
-    	const name = await this.bot.resolveUserNameFromId(randUser);
-    	this.bot.postMessage(data.channel, name);
-    } catch(e) {
-    	console.log(e);
-    }   
+    try {
+      const userData = await this.bot.getChannelById(data.channel);
+      const randUser = this.getRandomUser(userData);
+      const name = await this.bot.resolveUserNameFromId(randUser);
+      this.bot.postMessage(data.channel, name);
+    } catch (e) {
+      console.log(e);
+      this.bot.postMessage(
+        data.channel,
+        "Couldn't find anyone. Is this a private channel? If so I can't see the user list, ya bozo."
+      );
+    }
   }
 
   getRandomUser(userData) {
     return userData.members[
-		Math.floor( Math.random() * userData.members.length)
-	];
+      Math.floor(Math.random() * userData.members.length)
+    ];
   }
 
   help() {
     return "Usage: `?choose` will post a random person's name from the channel.";
   }
-
 };
