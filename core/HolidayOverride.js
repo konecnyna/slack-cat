1754c679bd12bfa42fbc19df89f1aec29e25f19f
Nick Konecny <nkonecny@etsy.com>
2018-06-26 19:01:47 -0400

    Holiday override

---
+++ core/HolidayOverride.js
@@ -0,0 +1,36 @@
+module.exports = class HolidayOverride {
+  
+  getOverride() {
+    if (this.validateDate(7, [3,4])) {
+      return this.fourthOfJulyOverride();
+    } else if (this.validateDate(12, [20,21,22,23,24,25,26])) {
+      return this.christmasOverride();
+    }
+
+    return false;
+  }
+
+  validateDate(month, days) {
+    const now = new Date();    
+    return (
+      days.includes(now.getDate()) &&
+      now.getMonth() == month - 1
+    );
+  }
+
+  fourthOfJulyOverride() {
+    return {
+      username: 'AmericaCat',
+      icon_emoji: ':flag-us:',
+      icon_url: '',
+    };
+  }
+
+  christmasOverride() {
+    return {
+      username: 'HolidayCat',
+      icon_emoji: ':christmas_tree:',
+      icon_url: '',
+    };  
+  }
+};
