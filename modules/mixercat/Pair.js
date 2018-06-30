ab7514942ceca8c6b8a07016b04c2fe44cc3d360
Nick Konecny <nkonecny@etsy.com>
2018-06-30 09:07:30 -0400

    Fixed dups

--- modules/mixercat/Pair.js
+++ modules/mixercat/Pair.js
@@ -2,27 +2,28 @@
 
 const Chooser = require('../choose/Chooser');
 const chooser = new Chooser();
+const Op = require('sequelize').Op;
 
 module.exports = class Pair {
-  async pairMembers(members, mixerCatModel) {    
+  async pairMembers(members, mixerCatModel) {
     const matches = [];
     const userMap = {};
-    for (let i=0; i < members.length; i++) {
-      const user = members[i];    
-      if (userMap[user]) {        
+    for (let i = 0; i < members.length; i++) {
+      const user = members[i];
+      if (userMap[user]) {
         continue;
       }
       userMap[user] = true;
-      const pairedUsers = await this.getPair(user, members, mixerCatModel);              
-      if (pairedUsers.length) {       
-        userMap[pairedUsers[1]] = true;        
-        matches.push(pairedUsers);                
-      } 
+      const pairedUsers = await this.getPair(user, members, mixerCatModel);
+      if (pairedUsers.length) {
+        userMap[pairedUsers[1]] = true;
+        matches.push(pairedUsers);
+      }
     }
 
     return matches;
   }
-  async getPair(userId, users, pairedTable) {  
+  async getPair(userId, users, pairedTable) {
     const potentialPairs = await this.getValidPairs(userId, users, pairedTable);
     if (!potentialPairs.length) {
       // User is out of matches.
@@ -31,7 +32,7 @@ module.exports = class Pair {
 
     const pairedMember = chooser.getRandomUser(potentialPairs);
     const result = await this.savePair(userId, pairedMember, pairedTable);
-    
+
     return [userId, pairedMember];
   }
 
@@ -39,34 +40,35 @@ module.exports = class Pair {
     const previousPairs = await pairedTable
       .findAll({
         where: {
-          user_id: userId,          
+          memberOne: userId,
+          memberTwo: {
+            [Op.or]: [userId],
+          },
         },
       })
       .map(it => {
-        return it.get('paired_with');
+        const memberOne = it.get('memberOne');
+        const memberTwo = it.get('memberTwo');
+        if (memberOne === userId) {
+          return memberTwo;
+        }
+
+        return memberOne;
       });
-   
+
     return users.filter(it => {
       return !previousPairs.includes(it) && it !== userId;
     });
   }
 
   async savePair(user, pairedUser, pairedTable) {
-    console.log("saving...", user, pairedUser)
     try {
       await pairedTable.create({
-        user_id: user,
-        paired_with: pairedUser,
+        memberOne: user,
+        memberTwo: pairedUser,
       });
-      await pairedTable.create({
-        user_id: pairedUser,
-        paired_with: user,
-      });
-      return true;
     } catch (e) {
       console.log('They have already paired DIS IS BAD!');
-      return false;
     }
   }
-
 };
