a8b32259fbfc01dcf4d25cb5b34b9f591240b879
Nick Konecny <nkonecny@stashinvest.com>
2018-07-06 12:41:15 -0400

    Added halloween:

--- core/HolidayOverride.js
+++ core/HolidayOverride.js
@@ -1,9 +1,10 @@
 module.exports = class HolidayOverride {
-  
   getOverride() {
-    if (this.validateDate(7, [3,4])) {
+    if (this.validateDate(7, [3, 4])) {
       return this.fourthOfJulyOverride();
-    } else if (this.validateDate(12, [20,21,22,23,24,25,26])) {
+    } else if (this.validateDate(10, [27, 28, 29, 30, 31])) {
+      return this.halloweenOverride();
+    } else if (this.validateDate(12, [20, 21, 22, 23, 24, 25, 26])) {
       return this.christmasOverride();
     }
 
@@ -11,11 +12,16 @@ module.exports = class HolidayOverride {
   }
 
   validateDate(month, days) {
-    const now = new Date();    
-    return (
-      days.includes(now.getDate()) &&
-      now.getMonth() == month - 1
-    );
+    const now = new Date();
+    return days.includes(now.getDate()) && now.getMonth() == month - 1;
+  }
+
+  halloweenOverride() {
+    return {
+      username: 'SpookyCat',
+      icon_emoji: ':jack_o_lantern:',
+      icon_url: '',
+    };
   }
 
   fourthOfJulyOverride() {
@@ -31,6 +37,6 @@ module.exports = class HolidayOverride {
       username: 'HolidayCat',
       icon_emoji: ':christmas_tree:',
       icon_url: '',
-    };  
+    };
   }
 };
