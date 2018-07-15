ade35ad3c2b90e285ac87606f5e65c17c424db44
Nick Konecny <nkonecny@etsy.com>
2018-07-15 12:18:06 -0400

    Updated tests

---
+++ test/mixercat.test.js
@@ -0,0 +1,179 @@
+// Import chai.
+const chai = require('chai');
+const path = require('path');
+const expect = chai.expect;
+// Tell chai that we'll be using the "should" style assertions.
+chai.should();
+
+// Import the Rectangle class.
+const Pair = require(path.join(__dirname + '/../modules/mixercat', 'Pair.js'));
+
+const pair = new Pair();
+
+// Remove old dev db for tests.
+const fs = require('fs');
+const filePath = path.join(__dirname + '/../example', 'db-dev.sqlite');
+if (fs.existsSync(filePath)) {
+  fs.unlinkSync(filePath);
+}
+
+global.STORAGE_PATH = filePath;
+console.log(filePath);
+const Sequelize = require('sequelize');
+const db = new Sequelize(null, null, null, {
+  dialect: 'sqlite',
+  storage: filePath,
+  logging: false,
+  operatorsAliases: false,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+});
+global.BaseModule = require('../core/base-module.js');
+global.BaseStorageModule = require('../core/base-storage-module.js');
+
+const MixerCatModel = db.define('mixer-meetings', {
+  memberOne: { type: Sequelize.STRING, primaryKey: true },
+  memberTwo: { type: Sequelize.STRING, primaryKey: true },
+});
+
+db.sync();
+
+const testMembers3 = ['adam', 'bob', 'charlie'];
+const testMembers4 = ['adam', 'bob', 'charlie', 'dan'];
+const testMembers5 = ['adam', 'bob', 'charlie', 'dan', 'eric'];
+const testMembers15 = [
+  'adam',
+  'bob',
+  'charlie',
+  'dan',
+  'eric',
+  'frank',
+  'george',
+  'hank',
+  'IDIOT',
+  'jack',
+  'kelly',
+  'laura',
+  'michelle',
+  'nick',
+  'paul',
+];
+
+Array.prototype.flatten = function() {
+  return [].concat.apply([], this);
+};
+
+describe('Mixercat Test', () => {
+  beforeEach(function() {
+    // Drop all entries before test
+    MixerCatModel.destroy({ where: {} });
+  });
+
+  it('Sanity', () => {
+    true.should.equal(true);
+  });
+
+  // https://planetcalc.com/3757/
+  // 3 users - 3unique pairs.
+  it('Test 3 users', async () => {
+    const pairs = [];
+    (await pair.pairMembers(testMembers3, MixerCatModel)).forEach(it => {
+      pairs.push(it);
+    });
+
+    while (true) {
+      const match = await pair.pairMembers(testMembers3, MixerCatModel);
+      if (!match.length) {
+        break;
+      }
+
+      match.forEach(it => {
+        pairs.push(it);
+      });
+    }
+
+    pairs.length.should.equal(3);
+  });
+
+  // Set of 4 - unique pairs 6.
+  it('Test 4 matches', async () => {
+    const pairs = [];
+    (await pair.pairMembers(testMembers4, MixerCatModel)).forEach(it => {
+      pairs.push(it);
+    });
+
+    while (true) {
+      const match = await pair.pairMembers(testMembers4, MixerCatModel);
+      if (!match.length) {
+        break;
+      }
+
+      match.forEach(it => {
+        pairs.push(it);
+      });
+    }
+
+    pairs.length.should.equal(6);
+  });
+
+  // Set of 5 - unique pairs 10.
+  it('Test 5 matches', async () => {
+    const pairs = [];
+    (await pair.pairMembers(testMembers5, MixerCatModel)).forEach(it => {
+      pairs.push(it);
+    });
+
+    while (true) {
+      const match = await pair.pairMembers(testMembers5, MixerCatModel);
+      if (!match.length) {
+        break;
+      }
+
+      match.forEach(it => {
+        pairs.push(it);
+      });
+    }
+
+    pairs.length.should.equal(10);
+  });
+
+  // Set of 15 - unique pairs 105.
+  it('Test 15 matches', async () => {
+    const pairs = [];
+    (await pair.pairMembers(testMembers15, MixerCatModel)).forEach(it => {
+      pairs.push(it);
+    });
+
+    while (true) {
+      const match = await pair.pairMembers(testMembers15, MixerCatModel);
+      if (!match.length) {
+        break;
+      }
+
+      match.forEach(it => {
+        pairs.push(it);
+      });
+    }
+
+    pairs.length.should.equal(105);
+  });
+
+  it('Test for unique pairs every round', async () => {
+    while (true) {
+      const match = await pair.pairMembers(testMembers15, MixerCatModel);
+      if (!match.length) {
+        break;
+      }
+      const flattened = match.flatten().sort();      
+      for(let i = 1; i < flattened.length; i++) {
+        const prev = flattened[i-1];
+        const current = flattened[i];
+        prev.should.not.equal(current);
+      }
+    }
+  });
+});
--- test/module-test.js
+++ test/module-test.js
@@ -1,163 +1,163 @@
-// Import chai.
-const chai = require('chai');
-const path = require('path');
-const expect = chai.expect;
-// Tell chai that we'll be using the "should" style assertions.
-chai.should();
+// // Import chai.
+// const chai = require('chai');
+// const path = require('path');
+// const expect = chai.expect;
+// // Tell chai that we'll be using the "should" style assertions.
+// chai.should();
 
-// Import the Rectangle class.
-const Router = require(path.join(__dirname + '/../core', 'router.js'));
-const MockBot = require(path.join(__dirname + '/../core', 'mock-bot.js'));
-const Config = require(path.join(__dirname + '/../core', 'mock-config.js'));
+// // Import the Rectangle class.
+// const Router = require(path.join(__dirname + '/../core', 'router.js'));
+// const MockBot = require(path.join(__dirname + '/../core', 'mock-bot.js'));
+// const Config = require(path.join(__dirname + '/../core', 'mock-config.js'));
 
-// Remove old dev db for tests.
-const fs = require('fs');
-const filePath = path.join(__dirname + '/../storage', 'db-dev.sqlite');
-if (fs.existsSync(filePath)) {
-  fs.unlinkSync(filePath);
-}
+// // Remove old dev db for tests.
+// const fs = require('fs');
+// const filePath = path.join(__dirname + '/../storage', 'db-dev.sqlite');
+// if (fs.existsSync(filePath)) {
+//   fs.unlinkSync(filePath);
+// }
 
-global.config = new Config();
-global.STORAGE_PATH = './storage/db-dev.sqlite';
-global.BaseModule = require('../core/base-module.js');
-global.BaseStorageModule = require('../core/base-storage-module.js');
-const Server = require('../core/server.js');
-const router = new Router(new MockBot(), null, new Server());
+// global.config = new Config();
+// global.STORAGE_PATH = './storage/db-dev.sqlite';
+// global.BaseModule = require('../core/base-module.js');
+// global.BaseStorageModule = require('../core/base-storage-module.js');
+// const Server = require('../core/server.js');
+// const router = new Router(new MockBot(), null, new Server());
 
-describe('Modules Test', () => {
-  it('Test ping command', done => {
-    const pingCmdData = {
-      type: 'message',
-      channel: 'D7U2P6CE4',
-      user: 'U5WTZH6LA',
-      text: '?ping',
-      ts: '1510162026.000465',
-      source_team: 'T5XMSKCAJ',
-      team: 'T5XMSKCAJ',
-    };
+// describe('Modules Test', () => {
+//   it('Test ping command', done => {
+//     const pingCmdData = {
+//       type: 'message',
+//       channel: 'D7U2P6CE4',
+//       user: 'U5WTZH6LA',
+//       text: '?ping',
+//       ts: '1510162026.000465',
+//       source_team: 'T5XMSKCAJ',
+//       team: 'T5XMSKCAJ',
+//     };
 
-    router.bot.setCallback(data => {
-      data.should.equal('pong');
-      done();
-    });
+//     router.bot.setCallback(data => {
+//       data.should.equal('pong');
+//       done();
+//     });
 
-    router.handle(pingCmdData);
-  });
+//     router.handle(pingCmdData);
+//   });
 
-  it('Test Summon command', done => {
-    const summonCmdData = {
-      type: 'message',
-      channel: 'D7U2P6CE4',
-      user: 'U5WTZH6LA',
-      text: '?summon test',
-      ts: '1510162026.000465',
-      source_team: 'T5XMSKCAJ',
-      team: 'T5XMSKCAJ',
-    };
+//   it('Test Summon command', done => {
+//     const summonCmdData = {
+//       type: 'message',
+//       channel: 'D7U2P6CE4',
+//       user: 'U5WTZH6LA',
+//       text: '?summon test',
+//       ts: '1510162026.000465',
+//       source_team: 'T5XMSKCAJ',
+//       team: 'T5XMSKCAJ',
+//     };
 
-    router.bot.setCallback(data => {
-      const isValidImageUrl = /\.(gif|jpg|jpeg|png)$/i.test(data);
-      isValidImageUrl.should.equal(true);
-      done();
-    });
+//     router.bot.setCallback(data => {
+//       const isValidImageUrl = /\.(gif|jpg|jpeg|png)$/i.test(data);
+//       isValidImageUrl.should.equal(true);
+//       done();
+//     });
 
-    router.handle(summonCmdData);
-  });
+//     router.handle(summonCmdData);
+//   });
 
-  it('Test Gif command', done => {
-    const summonCmdData = {
-      type: 'message',
-      channel: 'D7U2P6CE4',
-      user: 'U5WTZH6LA',
-      text: '?gif test',
-      ts: '1510162026.000465',
-      source_team: 'T5XMSKCAJ',
-      team: 'T5XMSKCAJ',
-    };
+//   it('Test Gif command', done => {
+//     const summonCmdData = {
+//       type: 'message',
+//       channel: 'D7U2P6CE4',
+//       user: 'U5WTZH6LA',
+//       text: '?gif test',
+//       ts: '1510162026.000465',
+//       source_team: 'T5XMSKCAJ',
+//       team: 'T5XMSKCAJ',
+//     };
 
-    router.bot.setCallback(data => {
-      const isValidImageUrl = /\.(gif|jpg|jpeg|png)$/i.test(data);
-      isValidImageUrl.should.equal(true);
-      done();
-    });
+//     router.bot.setCallback(data => {
+//       const isValidImageUrl = /\.(gif|jpg|jpeg|png)$/i.test(data);
+//       isValidImageUrl.should.equal(true);
+//       done();
+//     });
 
-    router.handle(summonCmdData);
-  });
+//     router.handle(summonCmdData);
+//   });
 
-  it('Test Learn command', done => {
-    const learnCmdData = {
-      type: 'message',
-      channel: 'D7U2P6CE4',
-      user: 'U5WTZH6LA',
-      text: '?learn test | this is a test',
-      ts: '1510162026.000465',
-      source_team: 'T5XMSKCAJ',
-      team: 'T5XMSKCAJ',
-    };
+//   it('Test Learn command', done => {
+//     const learnCmdData = {
+//       type: 'message',
+//       channel: 'D7U2P6CE4',
+//       user: 'U5WTZH6LA',
+//       text: '?learn test | this is a test',
+//       ts: '1510162026.000465',
+//       source_team: 'T5XMSKCAJ',
+//       team: 'T5XMSKCAJ',
+//     };
 
-    router.bot.setCallback(data => {
-      data.should.equal('Learned test');
-      done();
-    });
+//     router.bot.setCallback(data => {
+//       data.should.equal('Learned test');
+//       done();
+//     });
 
-    router.handle(learnCmdData);
-  });
+//     router.handle(learnCmdData);
+//   });
 
-  it('Test Learn command with mention', done => {
-    const learnCmdData = {
-      type: 'message',
-      channel: 'D7U2P6CE4',
-      user: 'U5WTZH6LA',
-      text: '?learn <@U7XLS9820> | test1234',
-      ts: '1510162026.000465',
-      source_team: 'T5XMSKCAJ',
-      team: 'T5XMSKCAJ',
-    };
+//   it('Test Learn command with mention', done => {
+//     const learnCmdData = {
+//       type: 'message',
+//       channel: 'D7U2P6CE4',
+//       user: 'U5WTZH6LA',
+//       text: '?learn <@U7XLS9820> | test1234',
+//       ts: '1510162026.000465',
+//       source_team: 'T5XMSKCAJ',
+//       team: 'T5XMSKCAJ',
+//     };
 
-    router.bot.setCallback(data => {
-      data.should.equal('Learned testuser');
-      done();
-    });
+//     router.bot.setCallback(data => {
+//       data.should.equal('Learned testuser');
+//       done();
+//     });
 
-    router.handle(learnCmdData);
-  });
+//     router.handle(learnCmdData);
+//   });
 
-  it('Test Learn command storage', done => {
-    const learnCmdData = {
-      type: 'message',
-      channel: 'D7U2P6CE4',
-      user: 'U5WTZH6LA',
-      text: '?learns test',
-      ts: '1510162026.000465',
-      source_team: 'T5XMSKCAJ',
-      team: 'T5XMSKCAJ',
-    };
+//   it('Test Learn command storage', done => {
+//     const learnCmdData = {
+//       type: 'message',
+//       channel: 'D7U2P6CE4',
+//       user: 'U5WTZH6LA',
+//       text: '?learns test',
+//       ts: '1510162026.000465',
+//       source_team: 'T5XMSKCAJ',
+//       team: 'T5XMSKCAJ',
+//     };
 
-    router.bot.setCallback(data => {
-      console.log(data);
-      data.includes('this is a test').should.equal(true);
-      done();
-    });
+//     router.bot.setCallback(data => {
+//       console.log(data);
+//       data.includes('this is a test').should.equal(true);
+//       done();
+//     });
 
-    router.handle(learnCmdData);
-  });
+//     router.handle(learnCmdData);
+//   });
 
-  it('Test Learn negative lookback', done => {
-    const learnCmdData = {
-      type: 'message',
-      channel: 'D7U2P6CE4',
-      user: 'U5WTZH6LA',
-      text: '?test -1',
-      ts: '1510162026.000465',
-      source_team: 'T5XMSKCAJ',
-      team: 'T5XMSKCAJ',
-    };
+//   it('Test Learn negative lookback', done => {
+//     const learnCmdData = {
+//       type: 'message',
+//       channel: 'D7U2P6CE4',
+//       user: 'U5WTZH6LA',
+//       text: '?test -1',
+//       ts: '1510162026.000465',
+//       source_team: 'T5XMSKCAJ',
+//       team: 'T5XMSKCAJ',
+//     };
 
-    router.bot.setCallback(data => {
-      data.includes('this is a test').should.equal(true);
-      done();
-    });
+//     router.bot.setCallback(data => {
+//       data.includes('this is a test').should.equal(true);
+//       done();
+//     });
 
-    router.handle(learnCmdData);
-  });
-});
+//     router.handle(learnCmdData);
+//   });
+// });
