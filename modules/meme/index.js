44d4eeb0b4a2f9a966a8a37419852aea34bf1e04
Nick Konecny <nkonecny@stashinvest.com>
2019-03-08 20:00:04 -0500

    MEMES

---
+++ modules/meme/index.js
@@ -0,0 +1,57 @@
+'use strict';
+const request = require('request');
+const regex = /"(.*?)"/g;
+
+module.exports = class Meme extends BaseModule {
+  async handle(data) {
+    if (!data.user_text) {
+      return;
+    }
+
+    
+    let group;
+    const text = [];
+    while(group = regex.exec(data.user_text)) {
+      console.log(group)
+      text.push(group[1]);      
+      if (text.length === 2) {
+        break;
+      }
+    }
+
+    const url = await this.imgflip({
+      template_id: '2743696',
+      username: 'daasdasda',
+      password: 'crossword',
+      text0: text[0], 
+      text1: text[1],
+    });
+
+    this.bot.postMessage(data.channel, url);
+  }
+
+async imgflip(params) {  
+  return new Promise((resolve, reject) => {
+    try {
+      request.post({
+        url: `https://api.imgflip.com/caption_image`,    
+        form: params    
+      }, (error, response, body) => {
+        resolve(JSON.parse(body).data.url);
+      });
+    } catch (e) {
+      reject(e);
+    }
+    
+  }); 
+}
+  
+
+  help() {
+    return '?meme <meme_name> "top_text" "bottom_text"';
+  }
+
+  aliases() {
+    return ['khan'];
+  }
+};
