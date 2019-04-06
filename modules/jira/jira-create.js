3ee95fd7166ad5d8b80c3738dfdf0b299007b5e2
Nick Konecny <nkonecny@etsy.com>
2019-04-06 10:46:19 -0400

    updoot

--- modules/jira/jira-create.js
+++ modules/jira/jira-create.js
@@ -1,51 +1,51 @@
-'use strict';
+'use strict'
 
 module.exports = class JiraCreate {
-  constructor(context) {
-    this.context = context;
+  constructor (context) {
+    this.context = context
   }
 
-  async createJiraTicket(body, jira) {
-    const userData = await this.context.bot.userDataPromise(body.user.id);
-    const currentUserRealName = userData.user.profile.real_name;
-    const currentUserEmail = userData.user.profile.email;
-    const shouldAssign = body.submission.assign;
-
+  async createJiraTicket (body, jira) {
+    const userData = await this.context.bot.userDataPromise(body.user.id)
+    const currentUserRealName = userData.user.profile.real_name
+    const currentUserEmail = userData.user.profile.email
+    const shouldAssign = body.submission.assign_select
+    console.log('1231232HI!')
     try {
       const payload = {
         fields: {
           project: {
-            key: body.submission.project,
+            key: body.submission.project
           },
           summary: body.submission.title,
           description: `${
             body.submission.description
-            }\n\n\nh2. Reporter:\n\n*${currentUserRealName} - (${currentUserEmail})*`,
+          }\n\n\nh2. Reporter:\n\n*${currentUserRealName} - (${currentUserEmail})*`,
           issuetype: {
-            name: 'Bug',
-          },
-        },
-      };
-      if (shouldAssign) {
-        // payload['fields']['assignee'] = this.findUser(currentUserEmail);
-        console.log(this.findUser(jira, currentUserEmail))
+            name: 'Bug'
+          }
+        }
       }
-      // return await jira.addNewIssue(payload);
+      if (shouldAssign) {
+        payload['fields']['assignee'] = currentUserEmail
+      }
+      console.log('HI!')
+      return await jira.addNewIssue(payload)
     } catch (e) {
-      console.log(e);
+      console.log(e)
     }
   }
 
-  async findUser(jira, email) {
-    const result = jira.searchUsers(email);
+  async findUser (jira, email) {
+    const result = jira.searchUsers(email)
     console.log(result)
   }
 
-  createRoutes(app, callbackId, projects) {
+  createRoutes (app, callbackId, projects) {
     app.post('/jira-create', (req, res) => {
       // extract the verification token, slash command text,
       // and trigger ID from payload
-      const { token, text, trigger_id } = req.body;
+      const { token, text, trigger_id } = req.body
 
       this.context.showDialog(
         {
@@ -58,43 +58,45 @@ module.exports = class JiraCreate {
               type: 'text',
               name: 'title',
               value: text,
-              hint: '30 second summary of the problem',
+              hint: 'Title of the problem'
             },
             {
               label: 'Project',
               type: 'select',
-              name: 'projec',
-              options: projetcts.map(({ key }) => ({
+              name: 'project',
+              options: projects.map(({ key }) => ({
                 label: key,
-                value: key,
-              })),
+                value: key
+              }))
             },
             {
               label: 'Assign to me',
               type: 'select',
               name: 'assign_select',
+              value: 'true',
               options: [
                 {
-                  label: "assign",
-                  value: true,
+                  label: 'true',
+                  value: 'true'
                 },
                 {
-                  label: "assign",
-                  value: false
+                  label: 'false',
+                  value: 'false'
                 }
-              ],
+              ]
             },
             {
               label: 'Description',
               type: 'textarea',
               name: 'description',
               optional: true,
-            },
-          ],
+              hint: '30 second summary of the problem'
+            }
+          ]
         },
         req.body,
         res
-      );
-    });
+      )
+    })
   }
-};
+}
