a3712be3ecc9347312d2cb2007cc7dc96b40464a
Mike Cornell <mike.cornell.009@gmail.com>
2019-08-12 11:14:48 -0400

    Tarot (#26)

---
+++ modules/tarot/index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const TarotCards = require('./cards.js');
+
+function responsePrefix() {
+  return ":crystal_ball::flower_playing_cards:: "
+}
+
+module.exports = class TarotModule extends BaseModule {
+
+  constructor(bot) {
+    super(bot);
+    this.tarotCards = new TarotCards();
+  }
+
+  async handle(data) {
+    if (data.user_text === undefined || data.user_text === "") {
+      this.bot.postMessage(data.channel, responsePrefix() + this.tarotCards.random());
+      return
+    }
+
+    this.bot.postMessage(data.channel, responsePrefix() + this.tarotCards.describe(data.user_text));
+  }
+
+  help() {
+    return 'Usage: `?tarot` for a single random card, `?tarot <card name>` for a specific card. EX: `?tarot The Magician`, `?tarot fool`'
+  }
+
+  aliases() {
+    return [':flower_playing_cards:']
+  }
+
+  getType() {
+      return [BaseModule.TYPES.MODULE];
+  }
+}
