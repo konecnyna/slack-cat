69bb93664ddd2e443af4c37e610def9c3bc47b1d
Mike Cornell <mike.cornell.009@gmail.com>
2019-10-09 17:46:46 -0400

    YEAH BABY WOO

--- modules/emojifont/index.js
+++ modules/emojifont/index.js
@@ -3,61 +3,49 @@
 const EmojiFont = require('./emojifont.js');
 const GoogleSpreadsheet = require("google-spreadsheet");
 const GoogleSheetsUtil = require('../google-sheets/spread-sheet-util');
-const sheets = config.getKey('google_sheets');
-const DOC_URL = `https://docs.google.com/spreadsheets/d/${sheets ? sheets.sheet_id : "" || ""}`
-const HELP_MSG = `
-Sorry couldn't find a sheet for this channel. Please visit: ${DOC_URL}
-and add a tab with the name being this channels id.
-`
+const HELP_MSG = "Sorry couldn't find the EmojiFont sheet.";
 const SHEET_ID = "1PXPOWgYbONZv5PRc3yT8rZ2AG6iA2qy5cKtdWp2wbaE";
+const SHEET_TAB_ID = "EmojiFontLookupTable"
 
 module.exports = class EmojifontModule extends BaseModule {
 
   constructor(bot) {
     super(bot);
-    this.emojiFont = new EmojiFont();
     this.googleSheetsUtil = new GoogleSheetsUtil();
-    if (sheets && sheets.sheet_id) {
-      this.doc = new GoogleSpreadsheet(sheets.sheet_id);
-    }
+    this.doc = new GoogleSpreadsheet(SHEET_ID);
   }
 
   async handle(data) {
-    console.log("handling!");
-
     if (!this.doc) {
-      console.log("heck");
       this.bot.postMessage(data.channel, HELP_MSG);
       return;
     }
 
-    console.log("have a doc ");
-
-    const sheet = await this.googleSheetsUtil.getSheetForChannel(this.doc, SHEET_ID);
-    console.log("got the sheet" + sheet);
+    const sheet = await this.googleSheetsUtil.getSheetForChannel(this.doc, SHEET_TAB_ID);
     if (!sheet) {
       this.bot.postMessageToThread(data.channel, HELP_MSG, data.ts);
       return;
     }
 
-    const rows = this.googleSheetsUtil.getRows(sheet);
-    console.log("rows " + rows);
+    const cells = await this.googleSheetsUtil.getCells(sheet);
+    const cleanedCells = cells.map((cell) => {
+      return {
+        x: cell.col,
+        y: cell.row,
+        value: cell.value
+      };
+    });
+    const emojiFont = new EmojiFont(cleanedCells);
 
-    const cells = this.googleSheetsUtil.getCells(sheet);
-    console.log("cells " + cells);
-
-    console.log("kick off");
     if (data.args.map(x => x.toLowerCase()).includes("--theworks")) {
-      this.bot.postMessage(data.channel, this.emojiFont.giveEmTheWorks());
+      this.bot.postMessage(data.channel, emojiFont.giveEmTheWorks());
     } else {
-      console.log("lets go");
-      this.bot.postMessageToThread(data.channel, this.emojiFont.emojify(data.user_text), data.ts);
-      console.log("over");
+      this.bot.postMessageToThread(data.channel, emojiFont.emojify(data.user_text), data.ts);
     }
   }
 
   help() {
-    return 'Usage: `?emojifont my phrase` to translate your phrase to emoji. Use ?emojifont --theworks for the works. Also aliased to ?emojify'
+    return 'Usage: `?ef my phrase` to translate your phrase to emoji. Use ?ef --theworks for the works. Also aliased to ?emojify and ?emojifont. Add more emojis at https://docs.google.com/spreadsheets/d/1PXPOWgYbONZv5PRc3yT8rZ2AG6iA2qy5cKtdWp2wbaE/edit#gid=0'
   }
 
   aliases() {
