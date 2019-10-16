c50e7ef08d27db80e5d24e20c9d3ea0828e9c93a
Mike Cornell <mike.cornell.009@gmail.com>
2019-10-16 16:33:08 -0400

    Royalty (#35)

---
+++ modules/royalty/royalty-util.js
@@ -0,0 +1,65 @@
+const formattedMemberName = (member) => {
+  return `${member.get('name')}, ${member.get('title')}`
+}
+
+module.exports = class RoyaltyUtil {
+  constructor(context) {
+    this.context = context;
+  }
+
+  async displayTitleForUser(user) {
+    const courtMember = await this.getRoyaltyModel().findOne({
+      where: {
+        name: user
+      }
+    });
+    if (courtMember === null) {
+      return `I know of no one named ${user} yet.`
+    }
+
+    return formattedMemberName(courtMember);
+  }
+
+  async displayFullCourtBoard(data) {
+    const courtMembers = await this.getRoyaltyModel().findAll({});
+
+    const fields = [];
+    courtMembers.forEach((member, index) => {
+      fields.push({
+        title: formattedMemberName(member),
+        short: false,
+      });
+    });
+
+    this.context.bot.postRawMessage(data.channel, {
+      icon_emoji: ':royalcat:',
+      username: 'RoyaltyCat',
+      attachments: [
+        {
+          color: '#7851a9',
+          fields: fields,
+        },
+      ],
+    });
+  }
+
+  async anointUser(userName, title) {
+    const courtMember = await this.context.upsert(
+      this.getRoyaltyModel(),
+      { where: { name: userName } },
+      {
+        name: userName,
+        title: title,
+      }, {
+        name: userName,
+        title: title
+      }
+    );
+
+    return formattedMemberName(courtMember);
+  }
+
+  getRoyaltyModel() {
+    return database.modelManager.getModel("royalties")
+  }
+};
