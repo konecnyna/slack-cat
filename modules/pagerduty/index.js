cfd44837155def441b7c0fac15f37d6741187a92
defkon <nkonecny@stashinvest.com>
2019-12-04 12:37:17 -0500

    New pager duty (#38)

--- modules/pagerduty/index.js
+++ modules/pagerduty/index.js
@@ -1,7 +1,21 @@
 'use strict';
-const request = require('request');
+const pdUtil = new (require('./pager-duty-util'))();
+const CronJob = require('cron').CronJob
 
 module.exports = class PagerDuty extends BaseModule {
+  constructor(bot) {
+    super(bot);
+    new CronJob(
+      "00 00 10 * * 1,2,3,4,5",
+      () => {
+        this.postToChannel("", "");
+      },
+      null,
+      true,
+      "America/New_York"
+    );
+  }
+
   async handle(data) {
     if (!this.isValidConfig()) {
       this.bot.postMessage(
@@ -11,103 +25,49 @@ module.exports = class PagerDuty extends BaseModule {
       return;
     }
 
-    const body = await this.getData(config.getKey('pager_duty_api').policies);
-    Object.keys(body).map(key => {
-      const fields = this.makeFields(body[key]);
-      const title = body[key][0].escalation_policy.summary;
-      this.postFieldsToChannel(data.channel, title, fields);
-    })
-    
+    if (!data.user_text.length) {
+      pdUtil.postBadInputError(this.bot, data);
+      return;
+    }
+
+    this.postToChannel(data.user_text, data.channel);
   }
 
-  help() {
-    return 'Pager duty command will return the on call list.';
+  async postToChannel(dataTeamName, channel) {
+    var pager_duty_api_json_obj = config.getKey('pager_duty_api')
+    pager_duty_api_json_obj["teams"].map(async it => {
+      // team_name and channel_name will be blank for cron job.
+      // Hence post the list to the channel_id, configured in config.json
+      var channelName = channel
+      if (channel === "") {
+        channelName = it.channel_id
+      }
+
+      // Either team_name should be blank i.e. cron job is executing OR
+      // team_name matches with the argument passed with the command, to print oncall for that team only.
+      if (dataTeamName === "" || dataTeamName === it.team_name) {
+        const body = await pdUtil.getData([it]);
+        const oncall = body[it.policy_id];
+        const fields = pdUtil.makeFields(oncall);
+        const title = oncall[0].escalation_policy.summary;
+        pdUtil.postFieldsToChannel(this.bot, channelName, title, fields);
+      }
+    });
   }
 
   isValidConfig() {
     return (
       config.getKey('pager_duty_api') &&
-      config.getKey('pager_duty_api').key &&
-      config.getKey('pager_duty_api').policies.length
+      config.getKey('pager_duty_api').key
     );
-  }
-
-  postFieldsToChannel(channel, title, fields) {
-    this.bot.postRawMessage(channel, {
-      icon_url:
-        'http://emojis.slackmojis.com/emojis/images/1467306358/628/pagerduty.png',
-      username: 'PagerDutyCat',
-      attachments: [
-        {
-          color: '#36a64f',
-          author_icon:
-            'https://cdn6.aptoide.com/imgs/9/6/a/96ac16c7e70a7cf636ad85f8b5c7d5b2_icon.png?w=256',
-          title: title,
-          fields: Object.values(fields).sort((a,b) => { return b.level - a.level }),
-          footer: ':fire: lets hope nothings on fire :fire:',
-        },
-      ],
-    });
-  }
-
-  makeFields(policy) {
-    const map = {};
-    policy.map(item => {
-      const key = `Level: ${item.escalation_level}`;
-      if (key in map) {
-        map[key].value += ', ' + item.user.summary;
-      } else {
-        map[key] = {
-          level: item.escalation_level,
-          title: 'Level ' + item.escalation_level,
-          value: item.user.summary,
-          short: false,
-        };
-      }
-    });
-
-    return map;
-  }
-
-  getData(policies) {
-    var options = {
-      url: 'https://api.pagerduty.com/oncalls',
-      headers: {
-        Authorization: 'Token token=' + config.getKey('pager_duty_api').key,
-        'Content-Type': 'application/json',
-        Accept: 'application/vnd.pagerduty+json;version=2',
-      },
-    };
-
-    return new Promise((resolve, reject) => {
-      request(options, (error, response, body) => {
-        if (error) {
-          reject(error);
-          console.error(error);
-          return;
-        }
-
-        const json = JSON.parse(body);
-        const policyGroups = {};
-        json.oncalls.map(item => {
-          for (var i = 0; i < policies.length; i++) {
-            if (policies[i].policy_id === item.escalation_policy.id) {
-              if (!policyGroups[policies[i].policy_id]) {
-                policyGroups[policies[i].policy_id] = [];
-              }
-
-              policyGroups[policies[i].policy_id].push(item);
-            }
-          }
-        });
-
-
-        resolve(policyGroups);
-      });
-    });
   }
 
   aliases() {
     return ['oncall'];
+  }
+
+
+  help() {
+    return "Usage `?oncallteam team_name`\nValid team_names : " + config.getKey('pager_duty_api')["teams"].map(obj => obj.team_name);
   }
 };
