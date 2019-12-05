fad3465fcf8ed0ed473318dc15b08656a86e5882
James Wong <jameswong@stsh-00894-james.wong-mbpr15t>
2019-12-05 11:02:47 -0500

    Add message action module type

--- core/module-loader.js
+++ core/module-loader.js
@@ -15,6 +15,7 @@ module.exports = class ModuleLoader {
     this.rawInputModules = {}
     this.dialogModules = {}
     this.serviceModules = {}
+    this.messageActionModules = {}
 
     this.registerModules()
   }
@@ -47,6 +48,15 @@ module.exports = class ModuleLoader {
         )
       } else if (moduleObj.getType().includes(BaseModule.TYPES.ENDPOINT)) {
         this.createRoutes(moduleObj)
+      }
+
+      if (moduleObj.getType().includes(BaseModule.TYPES.MESSAGE_ACTION)) {
+        this.addModules(
+          key,
+          moduleObj,
+          BaseModule.TYPES.MESSAGE_ACTION,
+          this.messageActionModules
+        )
       }
 
       // Add all modules types to cmd array.
