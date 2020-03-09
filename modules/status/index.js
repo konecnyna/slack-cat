f9b2645722497f1f736a25576c254aa380e6e52d
Nick Konecny <nkonecny@stashinvest.com>
2020-03-09 17:22:26 -0400

    updoot

---
+++ modules/status/index.js
@@ -0,0 +1,58 @@
+'use strict';
+const request = require('request');
+
+const key = config.getKey('stock');
+
+const DATA_HIGH_KEY = '2. high';
+const DATA_LOW_KEY = '3. low';
+const DATA_CLOSE_KEY = '4. close';
+
+
+const status = (icon, text) => { return { icon: icon, text: text } };
+const meeting = status(":date:", "In a meeting")
+const office = status(":office:", "In the office")
+const unavailable = status(":x:", "Unavailable")
+const vacation = status(":palm_tree:", "On vacation")
+const wfh = status(":house_with_garden:", "Work from home")
+
+const statusMap = {
+  "meeting": meeting,
+  "office": office,
+  "unavailable": unavailable,
+  "vacation": vacation,
+  "wfh": wfh
+}
+
+module.exports = class Stock extends BaseModule {
+  async handle(data) {
+    const status = statusMap[data.user_text];
+
+    if (!status) {
+      return this.bot.postMessageToThread(
+        data.channel,
+        this.help(),
+        data.ts
+      );
+    }
+
+    const { icon, text } = status
+    await this.bot.setStatus(data.user, icon, text);
+    this.bot.postMessageToThread(
+      data.channel,
+      `I've updated your status to: "${status.text}"`,
+      data.ts
+    );
+  }
+
+
+
+  help() {
+    return 'Update your status!\nExample:\n`?status wfh` or `?s wfh`\nStatuses you can use:\n' + Object.keys(statusMap).join("\n");
+  }
+
+
+
+  aliases() {
+    return ['s'];
+  }
+};
