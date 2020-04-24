d4c81b184f756292cc465a22ed517166738ba419
Nick Konecny <nkonecny@stashinvest.com>
2020-04-24 12:17:21 -0400

    Added slack cmd for pd

--- modules/welcomecat/index.js
+++ modules/welcomecat/index.js
@@ -60,8 +60,8 @@ module.exports = class WelcomeCat extends BaseStorageModule {
     }
   }
 
-  onDialogSubmit(body) {
-    this.welcomeDialog.onDialogSubmit(body);
+  async onDialogSubmit(body) {
+    await this.welcomeDialog.onDialogSubmit(body);
   }
 
   createRoutes(app) {
