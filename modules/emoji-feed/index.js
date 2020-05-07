36e6ed427c62397908a604a1a11d118d0ddaa4f6
Nick Konecny <nkonecny@stashinvest.com>
2020-05-07 16:46:49 -0400

    updoot

--- modules/emoji-feed/index.js
+++ modules/emoji-feed/index.js
@@ -4,8 +4,7 @@ const request = require('request-promise');
 const token = config.getKey('slack_access_token_oauth')
 const CronJob = require('cron').CronJob
 
-
-
+const CHANNEL = "CV689JSKF";
 module.exports = class EmojiFeed extends BaseStorageModule {
   constructor(bot) {
     super(bot);
@@ -21,12 +20,13 @@ module.exports = class EmojiFeed extends BaseStorageModule {
   }
 
   async handle(data) {
-
+    this.checkNewEmojis()
   }
 
-  checkNewEmojis() {
-    const response = await request(`https://slack.com/api/emoji.list?token=${token}`);
-    const { emoji } = JSON.parse(response);
+  async checkNewEmojis() {
+    const { emoji } = await request(`https://slack.com/api/emoji.list?token=${token}`, {
+      json: true
+    });
 
     const newList = [];
 
@@ -41,29 +41,44 @@ module.exports = class EmojiFeed extends BaseStorageModule {
         newList.push(key)
         return this.EmojiFeed.create({
           key: key,
-          value: emoji[key]
+          value: emoji[key],
+          date_added: new Date()
         })
       }
     })
 
     await Promise.all(promises)
+    console.log(newList)
     if (newList.length) {
       this.postNewEmojis(newList)
     }
   }
 
-  postNewEmojis(list) {
-    const messages = list.map(key => {
-      return `${key} - :${key}:`
+  async postNewEmojis(list) {
+    const fields = list.map(key => {
+      return {
+        value: `${key} - :${key}:`,
+        short: true
+      }
     })
-    await this.bot.postMessage("CV689JSKF", messages.join("\n"))
+
+    this.bot.postRawMessage(CHANNEL, {
+      attachments: [
+        {
+          title: 'New emojis:',
+          color: "#9C27B0",
+          fields: fields,
+        },
+      ],
+    });
   }
 
   registerSqliteModel() {
     this.EmojiFeed = this.db.define('emoji_feed', {
       key: { type: this.Sequelize.STRING, primaryKey: true },
       value: this.Sequelize.STRING,
-    });
+      date_added: this.Sequelize.DATE
+    }, { timestamps: false });
   }
 
 
