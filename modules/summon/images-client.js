400c0845867efd7d8fceefed7af9625b0dfe02c8
konecnyna <konecnyna@gmail.com>
2020-07-01 11:33:49 -0400

    Better

--- modules/summon/images-client.js
+++ modules/summon/images-client.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const axios = require('axios');
 
 Array.prototype.random = function () {
@@ -5,7 +6,9 @@ Array.prototype.random = function () {
 }
 
 const getImages = async (query, random) => {
-  const url = `https://www.bing.com/images/search?q=${query}`
+  const url = `https://www.bing.com/images/search?q=${query}&FORM=HDRSC2`
+  console.log(url)
+
   const header = { 'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36" }
   const { data } = await axios.get(url, { headers: header });
   const images = extractImages(data);
@@ -21,6 +24,7 @@ const extractImages = (data) => {
   const str = data.toString();
   let m;
   const result = [];
+  fs.writeFileSync('./test.html', data);
   while ((m = regex.exec(str)) !== null) {
     // This is necessary to avoid infinite loops with zero-width matches
     if (m.index === regex.lastIndex) {
@@ -36,4 +40,5 @@ const extractImages = (data) => {
   }
   return result;
 }
+
 module.exports.getImages = getImages
