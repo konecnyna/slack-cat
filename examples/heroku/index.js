e0c5540fe63f5a664a47cea354117eec8d0ffe75
defkon <konecnyna@gmail.com>
2020-09-09 15:30:06 -0400

    updoot

--- examples/heroku/index.js
+++
@@ -1,9 +0,0 @@
-const SlackCat = require("../../index.js");
-const Path = require("path");
-
-const configPath = Path.join(__dirname, "config.json");
-const dbPath = Path.join(__dirname, "db.sqlite");
-const modules = Path.join(__dirname, "/modules");
-
-// No modules in example.
-new SlackCat("", configPath, dbPath).start();
