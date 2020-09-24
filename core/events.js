e4280a2be22bae512ef36927b59bff00f2756d51
defkon <konecnyna@gmail.com>
2020-09-24 13:37:57 -0400

    [FEATURE] Added event bus to listen for startup"

---
+++ core/events.js
@@ -0,0 +1,38 @@
+const subscriptions = {}
+const EventTypes = {
+  StartingUp: {}
+}
+
+const subscribe = (eventType, callback) => {
+  const id = getIdGenerator()
+
+  if (!subscriptions[eventType])
+    subscriptions[eventType] = {}
+
+  subscriptions[eventType][id] = callback
+
+  return {
+    unsubscribe: () => {
+      delete subscriptions[eventType][id]
+      if (Object.keys(subscriptions[eventType]).length === 0) delete subscriptions[eventType]
+    }
+  }
+}
+
+const publish = (eventType, arg) => {
+  if (!subscriptions[eventType])
+    return
+
+  Object.keys(subscriptions[eventType]).forEach(key => subscriptions[eventType][key](arg))
+}
+
+const getIdGenerator = () => {
+  let lastId = 0
+
+  return function getNextUniqueId() {
+    lastId += 1
+    return lastId
+  }
+}
+
+module.exports = { publish, subscribe, EventTypes }
--- examples/index.js
+++ examples/index.js
@@ -1,8 +1,12 @@
-const SlackCat = require('../index');
+const { SlackCat, SlackCatEvents } = require('../index');
 const Path = require('path');
 
 const configPath = Path.join(__dirname, 'config.json');
 const dbPath = Path.join(__dirname, 'db.sqlite');
+
+SlackCatEvents.subscribe(SlackCatEvents.EventTypes.SetupComplete, (args) => {
+  console.log("Slackcat is now active!");
+})
 
 // No modules in example.
 new SlackCat('', configPath, dbPath).start();
