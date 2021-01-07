4b8cffb6455bc1446b337b859fd8e534f9f46392
ReleaseCat <slackcat@stashinvest.com>
2021-01-07 11:16:24 -0500

    [radar] Updated to new earl.

---
+++ modules/radar/index.js
@@ -0,0 +1,200 @@
+'use strict';
+
+const radars = [
+  {
+    "value": "usa",
+    "text": "United States"
+  },
+  {
+    "value": "bfl",
+    "text": "CA - Bakersfield"
+  },
+  {
+    "value": "bgm",
+    "text": "NY - Binghamton"
+  },
+  {
+    "value": "bis",
+    "text": "ND - Bismarck"
+  },
+  {
+    "value": "bml",
+    "text": "NH - Berlin"
+  },
+  {
+    "value": "bro",
+    "text": "TX - Brownsville"
+  },
+  {
+    "value": "bwg",
+    "text": "KY - Bowling Green"
+  },
+  {
+    "value": "cad",
+    "text": "MI - Cadillac"
+  },
+  {
+    "value": "clt",
+    "text": "NC - Charlotte"
+  },
+  {
+    "value": "csg",
+    "text": "GA - Columbus"
+  },
+  {
+    "value": "day",
+    "text": "OH - Dayton"
+  },
+  {
+    "value": "den",
+    "text": "CO - Denver"
+  },
+  {
+    "value": "dsm",
+    "text": "IA - Des Moines"
+  },
+  {
+    "value": "eyw",
+    "text": "FL - Key West"
+  },
+  {
+    "value": "fcx",
+    "text": "VA - Roanoke"
+  },
+  {
+    "value": "hfd",
+    "text": "CT - Hartford"
+  },
+  {
+    "value": "jef",
+    "text": "MO - Jefferson City"
+  },
+  {
+    "value": "law",
+    "text": "OK - Lawton"
+  },
+  {
+    "value": "lbf",
+    "text": "NE - North Platte"
+  },
+  {
+    "value": "lit",
+    "text": "AR - Little Rock"
+  },
+  {
+    "value": "lwt",
+    "text": "MT - Lewistown"
+  },
+  {
+    "value": "msy",
+    "text": "LA - New Orleans"
+  },
+  {
+    "value": "myl",
+    "text": "ID - McCall"
+  },
+  {
+    "value": "pie",
+    "text": "FL - Saint Petersburg"
+  },
+  {
+    "value": "pir",
+    "text": "SD - Pierre"
+  },
+  {
+    "value": "prc",
+    "text": "AZ - Prescott"
+  },
+  {
+    "value": "pvu",
+    "text": "UT - Provo"
+  },
+  {
+    "value": "rdm",
+    "text": "OR - Redmond"
+  },
+  {
+    "value": "riw",
+    "text": "WY - Riverton"
+  },
+  {
+    "value": "rno",
+    "text": "NV - Reno"
+  },
+  {
+    "value": "row",
+    "text": "NM - Roswell"
+  },
+  {
+    "value": "sat",
+    "text": "TX - San Antonio"
+  },
+  {
+    "value": "shd",
+    "text": "VA - Staunton"
+  },
+  {
+    "value": "sln",
+    "text": "KS - Salina"
+  },
+  {
+    "value": "spi",
+    "text": "IL - Springfield"
+  },
+  {
+    "value": "stc",
+    "text": "MN - Saint Cloud"
+  },
+  {
+    "value": "tiw",
+    "text": "WA - Tacoma"
+  },
+  {
+    "value": "tvr",
+    "text": "MS - Vicksburg"
+  }
+]
+
+
+const formatUrl = (value) => `https://s.w-x.co/staticmaps/wu/wxtype/county_loc/${value}/animate.png`
+module.exports = class Radar extends BaseModule {
+  async handle(data) {
+    const { channel_id } = data;
+    if (!data.user_text) {
+      return this.sendRadarMessage(channel_id, "https://s.w-x.co/staticmaps/wu/wxtype/none/usa/animate.png")
+    }
+
+    const input = data.user_text.toUpperCase();
+    const userRadar = radars.find(it => {
+      return it.text.includes(input);
+    })
+    if (userRadar) {
+      return this.sendRadarMessage(channel_id, formatUrl(userRadar.value))
+    }
+
+    this.bot.postMessage(
+      channel_id,
+      `Couldn't find ${data.user_text}.\nMaps:\n\`\`\`${radars.map(it => it.text).join('\n')}\`\`\``
+    );
+  }
+
+  sendRadarMessage(channelId, url) {
+    this.bot.postRawMessage(channelId, {
+      username: `RadarCat`,
+      attachments: [
+        {
+          color: '#a2a2a2',
+          fields: [],
+          image_url: url,
+        }
+      ]
+    });
+  }
+
+  help() {
+    return (
+      'Usage: `?radar` gives radar gif.\nAll radars: ' + radars.map(it => it.text).join('\n')
+    );
+  }
+
+};
--- modules/weather/index.js
+++ modules/weather/index.js
@@ -4,50 +4,10 @@ const DarkSky = require('dark-sky');
 const util = require('util');
 const GoogleGeocode = require('./google-geocode-api.js');
 const geocoder = new GoogleGeocode();
-
 const darksky = new DarkSky(config.getKey('darksky_api'));
 const botParams = {
   icon_emoji: ':cat:',
   username: 'WeatherCat',
-};
-
-const radarMap = {
-  AR: 'lit',
-  AZ: 'prc',
-  CA: 'bfl',
-  CO: 'den',
-  CT: 'hfd',
-  FL: 'eyw',
-  FL: 'pie',
-  GA: 'csg',
-  IA: 'dsm',
-  ID: 'myl',
-  IL: 'spi',
-  KS: 'sln',
-  KY: 'bwg',
-  LA: 'msy',
-  MI: 'cad',
-  MN: 'stc',
-  MO: 'jef',
-  MS: 'tvr',
-  MT: 'lwt',
-  NC: 'clt',
-  ND: 'bis',
-  NE: 'lbf',
-  NH: 'bml',
-  NM: 'row',
-  NV: 'rno',
-  NY: 'bgm',
-  OH: 'day',
-  OK: 'law',
-  OR: 'rdm',
-  SD: 'pir',
-  TX: 'bro',
-  TX: 'sat',
-  UT: 'pvu',
-  VA: 'fcx',
-  WA: 'tiw',
-  WY: 'riw',
 };
 
 const DAYS = [
@@ -62,11 +22,6 @@ const DAYS = [
 
 module.exports = class Weather extends BaseModule {
   async handle(data) {
-    if (this.aliases().includes(data.cmd)) {
-      this.handleRadar(data);
-      return;
-    }
-
     if (!config.getKey('darksky_api') || !config.getKey('google_geocode_api')) {
       this.bot.postMessage(
         data.channel,
@@ -101,41 +56,11 @@ module.exports = class Weather extends BaseModule {
       });
   }
 
-  handleRadar(data) {
-    let img = '';
-    if (!data.user_text) {
-      this.bot.postMessage(
-        data.channel,
-        `http://images.intellicast.com/WxImages/RadarLoop/usa_None_anim.gif?time=${Date.now()}`
-      );
-
-      return;
-    }
-
-    const location = radarMap[data.user_text.toUpperCase()];
-    if (location) {
-      img = `http://images.intellicast.com/WxImages/RadarLoop/${location}_None_anim.gif?time=${Date.now()}`;
-      this.bot.postMessage(data.channel, img);
-      return;
-    }
-
-    this.bot.postMessage(
-      data.channel,
-      `Couldn't find ${data.user_text}. Maps: \`\`\`${Object.keys(
-        radarMap
-      ).join('\n')}\`\`\``
-    );
-  }
-
   help() {
     return (
       'Usage: `?weather` gives current forecast for NYC. More cmds: ' +
       this.aliases()
     );
-  }
-
-  aliases() {
-    return ['us-radar', 'ne-radar', 'nyc-radar', 'radar'];
   }
 
   sendFormattedWeather(data, weatherData, location) {
