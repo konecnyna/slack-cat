0514e94bcfad340a9cb1542ea0fb5842947063b1
konecnyna <konecnyna@gmail.com>
2021-04-14 16:48:57 -0400

    [anniversary] update text and bump

--- modules/channel/index.js
+++ modules/channel/index.js
@@ -1,6 +1,6 @@
-'use strict';
-const CronJob = require('cron').CronJob
-const ChannelAnniversaries = require('./channel-anniversaries');
+"use strict";
+const CronJob = require("cron").CronJob;
+const ChannelAnniversaries = require("./channel-anniversaries");
 
 module.exports = class Channel extends BaseModule {
   constructor(bot) {
@@ -9,66 +9,66 @@ module.exports = class Channel extends BaseModule {
     new CronJob(
       "00 45 7 * * *",
       () => {
-        this.checkChannelAnniversaries()
+        this.checkChannelAnniversaries();
       },
       null,
       true,
-      'America/New_York'
-    )
+      "America/New_York",
+    );
   }
 
   async handle(data) {
-    if (data.cmd.includes('anniversary')) {
+    if (data.cmd.includes("anniversary")) {
       return this.getChannelAnniversary(data.channel);
     }
 
     this.bot.postMessage(
       data.channel,
-      `This channel's id is: \`${data.channel}\``
+      `This channel's id is: \`${data.channel}\``,
     );
   }
 
   async checkChannelAnniversaries() {
-    const channels = await this.anniversaries.getAnniversaries()
-    channels.map(channel => {
-      this.sendAnniversaryMsg(channel.id, channel.years_old)
-    })
+    const channels = await this.anniversaries.getAnniversaries();
+    channels.map((channel) => {
+      this.sendAnniversaryMsg(channel.id, channel.years_old);
+    });
   }
 
   async getChannelAnniversary(channelId) {
     const data = await this.anniversaries.getChannelAnniversary(channelId);
-    this.postMessage(channelId, `This channel was created on ${data.date}.\n\nIt's anniversary is ${data.days_til_anniversary} days away!`);
+    this.postMessage(
+      channelId,
+      `<#${channelId}> was created on ${data.date}.\n\nIt's anniversary is ${data.days_til_anniversary} days away!`,
+    );
   }
 
   async sendAnniversaryMsg(channelId, diff) {
-    let msg = ""
-    if (diff > 1) {
-      msg = `This channel is ${diff} years old today. \n\nHappy anniversary y'all!\n:birthday: :confetti_ball: :gift: :confetti_ball: :birthday:`
-    } else {
-      msg = "This channel is 1 year older today. \n\nHappy anniversary y'all!\n:birthday: :confetti_ball: :gift: :confetti_ball: :birthday:";
-    }
-
+    let msg =
+      `Happy digital creation day to you <#${channelId}>! We're all so proud of you for reaching ${diff} year${
+        diff > 1 ? "s" : ""
+      } old.\n\nThis cake is not a lie... :cake:`;
     this.postMessage(channelId, msg);
   }
 
   postMessage(channelId, msg) {
     this.bot.postRawMessage(channelId, {
-      icon_emoji: ':birthday:',
-      username: 'AnniversaryCat',
+      icon_emoji: ":birthday:",
+      username: "AnniversaryCat",
       attachments: [
         {
-          color: '#FEB3DD',
+          color: "#FEB3DD",
           author_name: msg,
-        }
-      ]
-    })
+        },
+      ],
+    });
   }
 
   aliases() {
-    return ['anniversary'];
+    return ["anniversary"];
   }
 
   help() {
-    return 'Get current channels id';
+    return "Get current channels id";
   }
 };
