e98ae44aadee86e3f91bc961faabb053360a2ba3
konecnyna <konecnyna@gmail.com>
2021-08-02 13:33:50 -0400

    [channel] update bot limit to 1000 to avoid pagination

--- core/slack-cat-bot.js
+++ core/slack-cat-bot.js
@@ -1,50 +1,50 @@
-'use strict'
-const extend = require('extend')
-const { WebClient } = require('@slack/web-api');
-const HolidayOverride = require('./HolidayOverride')
+"use strict";
+const extend = require("extend");
+const { WebClient } = require("@slack/web-api");
+const HolidayOverride = require("./HolidayOverride");
 
 module.exports = class SlackCatBot {
   constructor(data) {
-    this.botInfo = data.self
-    this.web = new WebClient(config.getKey('slack_access_token'));
-    this.workSpace = new WebClient(config.getKey('slack_access_token_oauth'))
-    this.botParams = {}
-    this.setupBotParams()
+    this.botInfo = data.self;
+    this.web = new WebClient(config.getKey("slack_access_token"));
+    this.workSpace = new WebClient(config.getKey("slack_access_token_oauth"));
+    this.botParams = {};
+    this.setupBotParams();
   }
 
   setupBotParams() {
-    const name = config.getKey('bot_name')
-    this.icon_emoji = config.getKey('bot_emoji')
-    this.icon_url = config.getKey('bot_icon_url')
+    const name = config.getKey("bot_name");
+    this.icon_emoji = config.getKey("bot_emoji");
+    this.icon_url = config.getKey("bot_icon_url");
 
     this.botParams = {
-      username: name || 'SlackCat'
-    }
+      username: name || "SlackCat",
+    };
 
     if (this.icon_url) {
       // url takes prority.
-      this.botParams['icon_url'] = this.icon_url
+      this.botParams["icon_url"] = this.icon_url;
     } else {
-      this.botParams['icon_emoji'] = this.icon_emoji || ':cat:'
+      this.botParams["icon_emoji"] = this.icon_emoji || ":cat:";
     }
 
     // Override slackcat for some fun holidays!
-    this.defaultParams = this.botParams
-    if (config.getKey('holiday_override')) {
-      this.overrideBotParams()
+    this.defaultParams = this.botParams;
+    if (config.getKey("holiday_override")) {
+      this.overrideBotParams();
       setInterval(() => {
-        this.overrideBotParams()
-      }, 60 * 1000 * 1)
+        this.overrideBotParams();
+      }, 60 * 1000 * 1);
     }
   }
 
   overrideBotParams() {
-    const holidayOverride = new HolidayOverride()
-    const override = holidayOverride.getOverride()
+    const holidayOverride = new HolidayOverride();
+    const override = holidayOverride.getOverride();
     if (override) {
-      this.botParams = override
+      this.botParams = override;
     } else {
-      this.botParams = this.defaultParams
+      this.botParams = this.defaultParams;
     }
   }
 
@@ -52,15 +52,15 @@ module.exports = class SlackCatBot {
     const params = extend(
       {
         text: text,
-        channel: id
+        channel: id,
       },
-      this.botParams
-    )
+      this.botParams,
+    );
 
     try {
-      return await this.web.chat.postMessage(params)
+      return await this.web.chat.postMessage(params);
     } catch (e) {
-      console.error('postMessage', e.getMessage())
+      console.error("postMessage", e.getMessage());
     }
   }
 
@@ -68,15 +68,15 @@ module.exports = class SlackCatBot {
     const params = extend(
       {
         text: text,
-        channel: id
+        channel: id,
       },
-      extras
-    )
+      extras,
+    );
 
     try {
-      return await this.web.chat.postMessage(params)
+      return await this.web.chat.postMessage(params);
     } catch (e) {
-      console.error('postMessage', e.getMessage())
+      console.error("postMessage", e.getMessage());
     }
   }
 
@@ -87,21 +87,21 @@ module.exports = class SlackCatBot {
         {
           color: color,
           title: title,
-          text: body
-        }
-      ]
-    }
+          text: body,
+        },
+      ],
+    };
 
     var params = extend(
       {
         channel: channel_id,
-        username: this.botParams.username
+        username: this.botParams.username,
       },
       botParams || {},
-      attachments
-    )
+      attachments,
+    );
 
-    return this.postRawMessage(channel_id, params)
+    return this.postRawMessage(channel_id, params);
   }
 
   async postMessageToThread(id, text, ts, params) {
@@ -110,110 +110,113 @@ module.exports = class SlackCatBot {
         text: text,
         channel: id,
         thread_ts: ts,
-        username: this.botParams.username
+        username: this.botParams.username,
       },
-      params || this.botParams
-    )
+      params || this.botParams,
+    );
 
-    return await this.web.chat.postMessage(params)
+    return await this.web.chat.postMessage(params);
   }
 
   async postMessageToThreadOrUpdate(id, text, ts, params) {
-    return await this.postMessageToThread(id, text, ts, params)
+    return await this.postMessageToThread(id, text, ts, params);
   }
 
   postRawMessage(channel_id, args) {
     var params = extend(
       {
         channel: channel_id,
-        username: this.botParams.username
+        username: this.botParams.username,
       },
-      args || {}
-    )
+      args || {},
+    );
 
-    if (params['icon_emoji'] === undefined && params['icon_url'] === undefined) {
+    if (
+      params["icon_emoji"] === undefined && params["icon_url"] === undefined
+    ) {
       if (this.icon_url) {
-        params['icon_url'] = this.icon_url
+        params["icon_url"] = this.icon_url;
       } else {
-        params['icon_emoji'] = this.icon_emoji
+        params["icon_emoji"] = this.icon_emoji;
       }
     }
 
-    return this.web.chat.postMessage(params)
+    return this.web.chat.postMessage(params);
   }
 
   async getUserNameFromId(user_id) {
     return await this.web.users.info({
-      user: user_id
-    })
+      user: user_id,
+    });
   }
 
   async resolveUserNameFromId(user_id) {
-    const randUserData = await this.userDataPromise(user_id)
+    const randUserData = await this.userDataPromise(user_id);
     return randUserData.user.profile.display_name
       ? randUserData.user.profile.display_name
-      : randUserData.user.profile.real_name
+      : randUserData.user.profile.real_name;
   }
 
   userDataPromise(user_id) {
     return this.web.users.info({
-      user: user_id
-    })
+      user: user_id,
+    });
   }
 
   postMessageToUser(userId, msg) {
     return this.web.conversations
       .open({
-        users: userId
+        users: userId,
       })
-      .then(res => {
+      .then((res) => {
         this.web.chat.postMessage({
           channel: res.channel.id,
-          text: msg
-        })
+          text: msg,
+        });
       })
-      .catch(console.error)
+      .catch(console.error);
   }
 
   postMessageToUsers(userList, msg) {
     return this.web.conversations
       .open({
-        users: userList.join(',')
+        users: userList.join(","),
       })
-      .then(res => {
+      .then((res) => {
         this.web.chat.postMessage({
           channel: res.channel.id,
-          text: msg
-        })
+          text: msg,
+        });
       })
-      .catch(console.error)
+      .catch(console.error);
   }
 
   getChannelMembers(channel) {
+    // limit under 1000 will result in pagination.
     return this.web.conversations
       .members({
         channel: channel,
-        limit: 500
+        limit: 1000,
       })
-      .then(res => {
-        return res.members
+      .then((res) => {
+        return res.members;
       })
-      .catch(console.error)
+      .catch(console.error);
   }
 
   getChannelById(channel) {
     return this.web.conversations
       .info({
-        channel: channel
+        channel: channel,
       })
-      .then(res => {
-        return res.channel
+      .then((res) => {
+        return res.channel;
       })
-      .catch(console.error)
+      .catch(console.error);
   }
 
   setModules(modules) {
-    this.modules = modules
+    this.modules = modules;
   }
 
   async getUserNameDisplayNameFromId(id) {
@@ -226,13 +229,13 @@ module.exports = class SlackCatBot {
       return null;
     }
 
-    const rgx = /<@([^\s|\<]+)>/g
+    const rgx = /<@([^\s|\<]+)>/g;
     const matches = rgx.exec(text);
     if (!matches) {
-      return null
+      return null;
     }
 
-    const slackUser = await this.getUserNameFromId(matches[1].toUpperCase())
+    const slackUser = await this.getUserNameFromId(matches[1].toUpperCase());
     return slackUser.user.name;
   }
 
@@ -242,8 +245,8 @@ module.exports = class SlackCatBot {
       profile: {
         "status_text": text,
         "status_emoji": icon,
-        "status_expiration": 0
-      }
-    })
+        "status_expiration": 0,
+      },
+    });
   }
-}
+};
