8e298903a0d148661762a73ef1bddddab1193642
Slackcat <slackcat@stash.com>
2023-04-12 12:20:56 -0400

    updoot

--- core/base-module.js
+++ core/base-module.js
@@ -2,8 +2,16 @@
 const axios = require('axios');
 const qs = require('querystring');
 
-module.exports = class BaseModule {
-  constructor(bot) {
+class Err extends Error {
+  constructor(message) {
+    super(message);
+    this.classLogName = this.constructor.name;
+  }
+}
+
+module.exports = class BaseModule extends Err {
+  constructor(bot, message) {
+    super(message)
     if (new.target === BaseModule) {
       throw new TypeError('Cannot construct Abstract instances directly');
     }
--- core/module-loader.js
+++ core/module-loader.js
@@ -22,96 +22,105 @@ module.exports = class ModuleLoader {
 
   registerModules() {
     // Core modules
-    const loadedModules = moduleResolver.loadModules(this.pathToModules)
-    Object.keys(loadedModules).forEach(key => {
-      const moduleObj = new loadedModules[key](this.bot)
-      if (!moduleObj) {
-        console.error('Failed to instaniate module: ' + key)
-        return
+    const moduleKeys = moduleResolver.loadModules(this.pathToModules)
+    for (const key in moduleKeys) {
+      try {
+        this.instantiateModule(moduleKeys[key], key)
+      } catch (e) {
+        console.error(key, (e && e.stack) || e)
       }
-
-      if (
-        config.getKey('modules_blacklist') &&
-        config.getKey('modules_blacklist').includes(key)
-      ) {
-        console.log('Skipping: ', key, ' in blacklist.')
-        return
-      }
-
-      if (moduleObj.getType().includes(BaseModule.TYPES.DIALOG)) {
-        this.createRoutes(moduleObj)
-        this.addModules(
-          key,
-          moduleObj,
-          BaseModule.TYPES.DIALOG,
-          this.dialogModules
-        )
-      } else if (moduleObj.getType().includes(BaseModule.TYPES.ENDPOINT)) {
-        this.createRoutes(moduleObj)
-      }
-
-      if (moduleObj.getType().includes(BaseModule.TYPES.MESSAGE_ACTION)) {
-        this.addModules(
-          key,
-          moduleObj,
-          BaseModule.TYPES.MESSAGE_ACTION,
-          this.messageActionModules
-        )
-      }
-
-      // Add all modules types to cmd array.
-      if (moduleObj.getType().includes(BaseModule.TYPES.MODULE)) {
-        this.modules[key] = moduleObj
-        moduleObj.aliases().map(alias => {
-          if (this.modules[alias]) {
-            console.error('************************************************')
-            console.error('* Warning: Overwriting [' + alias + '] alias. *')
-            console.error('************************************************')
-          }
-          this.modules[alias] = moduleObj
-        })
-      }
-
-      this.addModules(
-        key,
-        moduleObj,
-        BaseModule.TYPES.OVERFLOW_CMD,
-        this.overflowModules
-      )
-      this.addModules(
-        key,
-        moduleObj,
-        BaseModule.TYPES.REACTION,
-        this.reactionModules
-      )
-      this.addModules(
-        key,
-        moduleObj,
-        BaseModule.TYPES.MESSAGE_EDITED,
-        this.messageEditedModules
-      )
-      this.addModules(
-        key,
-        moduleObj,
-        BaseModule.TYPES.MEMBER_JOINED_CHANNEL,
-        this.memberJoinedModules
-      )
-      this.addModules(
-        key,
-        moduleObj,
-        BaseModule.TYPES.RAW_INPUT,
-        this.rawInputModules
-      )
-      this.addModules(
-        key,
-        moduleObj,
-        BaseModule.TYPES.SERVICE,
-        this.serviceModules
-      )
-    })
+    }
 
     // Models are all loaded so now we sync the database models.
     database.sync()
+  }
+
+
+  instantiateModule(className, key) {
+    const moduleObj = new className(this.bot)
+    if (!moduleObj) {
+      console.error('Failed to instaniate module: ' + key)
+      return
+    }
+
+    if (
+      config.getKey('modules_blacklist') &&
+      config.getKey('modules_blacklist').includes(key)
+    ) {
+      console.log('Skipping: ', key, ' in blacklist.')
+      return
+    }
+
+    if (moduleObj.getType().includes(BaseModule.TYPES.DIALOG)) {
+      this.createRoutes(moduleObj)
+      this.addModules(
+        key,
+        moduleObj,
+        BaseModule.TYPES.DIALOG,
+        this.dialogModules
+      )
+    } else if (moduleObj.getType().includes(BaseModule.TYPES.ENDPOINT)) {
+      this.createRoutes(moduleObj)
+    }
+
+    if (moduleObj.getType().includes(BaseModule.TYPES.MESSAGE_ACTION)) {
+      this.addModules(
+        key,
+        moduleObj,
+        BaseModule.TYPES.MESSAGE_ACTION,
+        this.messageActionModules
+      )
+    }
+
+    // Add all modules types to cmd array.
+    if (moduleObj.getType().includes(BaseModule.TYPES.MODULE)) {
+      this.modules[key] = moduleObj
+      moduleObj.aliases().map(alias => {
+        if (this.modules[alias]) {
+          console.error('************************************************')
+          console.error('* Warning: Overwriting [' + alias + '] alias. *')
+          console.error('************************************************')
+        }
+        this.modules[alias] = moduleObj
+      })
+    }
+
+    this.addModules(
+      key,
+      moduleObj,
+      BaseModule.TYPES.OVERFLOW_CMD,
+      this.overflowModules
+    )
+    this.addModules(
+      key,
+      moduleObj,
+      BaseModule.TYPES.REACTION,
+      this.reactionModules
+    )
+    this.addModules(
+      key,
+      moduleObj,
+      BaseModule.TYPES.MESSAGE_EDITED,
+      this.messageEditedModules
+    )
+    this.addModules(
+      key,
+      moduleObj,
+      BaseModule.TYPES.MEMBER_JOINED_CHANNEL,
+      this.memberJoinedModules
+    )
+    this.addModules(
+      key,
+      moduleObj,
+      BaseModule.TYPES.RAW_INPUT,
+      this.rawInputModules
+    )
+    this.addModules(
+      key,
+      moduleObj,
+      BaseModule.TYPES.SERVICE,
+      this.serviceModules
+    )
   }
 
   getModules() {
